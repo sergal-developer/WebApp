a661abe298652cb27855a139b86bebe56ec07399
sergio.gallegos <sergio.gallegos@live.com.mx>
2024-09-21 23:39:00 -0600

    upgrade angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-selector: 'app-root',
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  constructor() {
-  }
+  title = 'ALVA';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,8 @@
-const CONFIG = {
-  N : 100,
-  SPECTRUM: [
-    // "rgb(222,237,250)"
-    'rgb(176,212,243)',
-    'rgb(128,186,236)',
-    'rgb(77,158,228)',
-    'rgb(38,137,223)',
-    'rgb(0,116,217)',
-    'rgb(0,106,197)'
-    // "rgb(0,94,176)"
-    // "rgb(0,82,154)"
-    // "rgb(0,60,113)"
-  ]
-};
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-export default CONFIG;
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)]
+};
